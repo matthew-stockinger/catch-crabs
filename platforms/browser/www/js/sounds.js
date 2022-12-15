72801a0a554f5fda9a7d86d5c611354f1f425f5a
Matthew Stockinger <monkeygrp@gmail.com>
2022-12-15 16:28:11 -0600

    bailed on parcel

--- platforms/browser/www/js/sounds.js
+++ platforms/browser/www/js/sounds.js
@@ -1,11 +1,12 @@
-'use strict';
+"use strict";
 
 const Sounds = {
-  crabClick: new Howl({ src: ['audio/pop.mp3'] }),
-  crabMiss: new Howl({ src: ['audio/misspop1.mp3'] }),
-  hundo: new Howl({ src: ['audio/hundo1.mp3'] }),
+  // parceljs won't catch these sound files unless they are fetched using a JS URL constructor.
+  crabClick: new Howl({ src: ["audio/pop.mp3"] }),
+  crabMiss: new Howl({ src: ["audio/misspop1.mp3"] }),
+  hundo: new Howl({ src: ["audio/hundo1.mp3"] }),
 
   mute(muted) {
     Howler.mute(muted);
-  }
+  },
 };
