9f9a6116d890f7df6933fc32294ef58ff3b0639b
Matthew Stockinger <monkeygrp@gmail.com>
2022-12-27 09:03:00 -0600

    upgraded to android api 31

--- platforms/android/app/src/main/java/com/stockmobilesoftware/catchcrabs/MainActivity.java
+++ platforms/android/app/src/main/java/com/stockmobilesoftware/catchcrabs/MainActivity.java
@@ -20,6 +20,7 @@
 package com.stockmobilesoftware.catchcrabs;
 
 import android.os.Bundle;
+
 import org.apache.cordova.*;
 
 public class MainActivity extends CordovaActivity
--- platforms/browser/www/assets-working/archive/debug.js
+++
@@ -1,4 +0,0 @@
-if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-    console.log("setWebContentsDebuggingEnabled true");
-    WebView.setWebContentsDebuggingEnabled(true);
-}
